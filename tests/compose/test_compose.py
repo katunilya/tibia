18fd2a95a167939993da4e3d427df1d052baff9d
Ilya Katun <i.katun@tinkoff.ru>
2024-05-18 14:29:26 +0300

    feat[#19]: add Compose & AsyncCompose for function composition

---
+++ tests/compose/test_compose.py
@@ -0,0 +1,20 @@
+import pytest
+
+from pypeline.compose import Compose
+from tests.example_functions import add, add_async
+
+
+def test_call():
+    _compose = Compose(add(0))
+    assert _compose(10) == 10
+
+
+def test_then():
+    _compose = Compose(add(1)).then(add(2)).then(add(3))
+    assert _compose(10) == 10 + 1 + 2 + 3
+
+
+@pytest.mark.asyncio
+async def test_then_async():
+    _compose = Compose(add(1)).then(add(2)).then_async(add_async(3))
+    assert await _compose(10) == 10 + 1 + 2 + 3
