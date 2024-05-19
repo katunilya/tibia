02c923822e157065e16eaf9a32c3975e1aef8e82
Ilya Katun <i.katun@tinkoff.ru>
2024-05-19 16:44:25 +0300

    feat[#25]: add passing

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+import pytest
+
+from pypeline.utils import async_identity, identity, passing
+
+
+def test_identity():
+    assert identity(3) == 3
+
+
+@pytest.mark.asyncio
+async def test_async_identity():
+    assert await async_identity(3) == 3
+
+
+def test_passing():
+    def returns_none(value: int): ...
+
+    assert passing(returns_none)(0) == 0
